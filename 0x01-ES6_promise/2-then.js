a63c6eecd8219c2918fd19d5c06cfcbb32cd86d5
mojtaba <mojtaba.mohamed187@gmail.com>
2024-05-29 14:31:04 +0300

    fix thrown error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,10 +3,6 @@ export default function handleResponseFromAPI(promise) {
     .then(() => (
       { status: 200, body: 'Success' }
     ))
-    .catch(() => (
-      new Error('')
-    ))
-    .finally(() => (
-      console.log('Got a response from the API')
-    ));
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
