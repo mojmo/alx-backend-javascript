0f39cb452176bc25179cbf10357a354be908b781
mojtaba <mojtaba.mohamed187@gmail.com>
2024-05-31 04:11:54 +0300

    Add task '6. Inheritance'

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call parent constructor with sqft
+
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
