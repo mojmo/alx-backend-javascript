d52185411487a501678bfb5bfa8592935d1b7bbf
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-04 14:51:24 +0300

    check if there is undefined values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') { return ''; }
+  if (startString === '' || startString === undefined) { return ''; }
 
   const filteredValues = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
+    .filter((value) => (value !== undefined ? value.startsWith(startString) : ''))
+    .map((value) => (value !== undefined ? value.slice(startString.length) : ''));
 
   return filteredValues.join('-');
 }
