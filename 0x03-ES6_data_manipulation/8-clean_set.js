ed97b9f5dabf55364345ecb87beaa21c2846708a
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-04 14:55:44 +0300

    use spread operator instead of Array.from()

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (startString === '' || startString === undefined) { return ''; }
 
-  const filteredValues = Array.from(set)
+  const filteredValues = [...set]
     .filter((value) => (value !== undefined ? value.startsWith(startString) : ''))
     .map((value) => (value !== undefined ? value.slice(startString.length) : ''));
 
