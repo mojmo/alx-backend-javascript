19721f1e6b0d5d8ce2599d49ce5f8a5d4efeb5dc
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-06 03:33:15 +0300

    add studentClass

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,30 @@ interface PrintTeacherFunction {
 function printTeacher(firstName: string, lastName: string): string {
     return `${firstName[0]}. ${lastName}`;
 }
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClass {
+    constructor(public firstName: string, public lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
