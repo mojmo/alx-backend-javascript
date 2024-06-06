a3a44dfb8bd5fd146ecf1b310e5fcf9b4b018bfc
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-06 03:39:22 +0300

    Add task '5. Advanced types Part 1'

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,52 @@
+// Interface for Director methods
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+// Interface for Teacher methods
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Director class implementing DirectorInterface
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+// Teacher class implementing TeacherInterface
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+}
+
+// Function to create employee based on salary
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
