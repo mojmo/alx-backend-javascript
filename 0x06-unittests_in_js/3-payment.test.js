a3c3ce015c4206f455989fbf6deff0c41fad7fa0
mojtaba <mojtaba.mohamed187@gmail.com>
2024-07-25 21:37:42 +0300

    use spy to monitor the behavior of a function

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+// 3-payment.test.js
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with correct arguments', function() {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    spy.restore();
+  });
+});
